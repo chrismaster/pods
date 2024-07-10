06072677e02dfc1c4d49009a0ece77c0ee0b0ca6
chrismaster <chrismaster360@gmail.com>
2024-07-10 15:56:36 +0200

    Update adminscript.sh

--- adminscript.sh
+++ adminscript.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 # vi: ft=sh
 mkdir -p ~/.local/bin
+for adminfile in pCreateUser pDeleteUser pList pLogin pPrune pUpdateCheck
+do
+    (cd ~/.local/bin && curl -sSLO https://githubusercontent.com/chrismaster/pods/main/adminscripts/$adminfile && chmod 775 $adminfile)
+done
