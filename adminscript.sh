03d5e8ea108e4ee80d273d8e0a41f19f829d10f9
chrismaster <chrismaster360@gmail.com>
2025-03-31 12:07:52 +0200

    Update adminscript.sh

--- adminscript.sh
+++ adminscript.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # vi: ft=sh
 mkdir -p ~/.local/bin
-for adminfile in pCreateUser pDeleteUser pList pLogin pPrune pUpdateCheck
+for adminfile in pCreateUser pDeleteUser pList pLogin pPrune pUpdateCheck pUpdateAll
 do
     (cd ~/.local/bin && curl -sSLO https://raw.githubusercontent.com/chrismaster/pods/main/adminscripts/$adminfile && chmod 775 $adminfile)
 done
 
-for userfile in pBuild pContainer pContainerNetwork pExec pLog pPod pRestart pStatus pStop pUpdate pUpdateAll
+for userfile in pBuild pContainer pContainerNetwork pExec pLog pPod pRestart pStatus pStop pUpdate
 do
     (cd /usr/local/bin && sudo curl -sSLO https://raw.githubusercontent.com/chrismaster/pods/main/userscripts/$userfile && sudo chmod 775 $userfile)
 done
